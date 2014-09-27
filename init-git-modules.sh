4e883e3b45e5853bac037bd3c6da741aa882ff40
Anton Wilhelm <timaschew@gmail.com>
2014-09-27 12:54:26 +0200

    add init script

---
+++ init-git-modules.sh
@@ -0,0 +1,27 @@
+modules[0]="build.js"
+modules[1]="builder2.js"
+modules[2]="builder-autoprefixer"
+modules[3]="consoler.js"
+modules[4]="downloader.js"
+modules[5]="flatten.js"
+modules[6]="ls.js"
+modules[7]="manifest.js"
+modules[8]="outdated.js"
+modules[9]="pin.js"
+modules[10]="remotes.js"
+modules[11]="require2"
+modules[12]="resolver.js"
+modules[13]="search.js"
+modules[14]="update.js"
+modules[15]="validtor.js"
+modules[16]="watcher.js"
+modules[17]="component"
+
+modules[31]="guide"
+modules[32]="spec"
+
+for i in ${modules[@]}
+do
+    url="https://github.com/componentjs/${i}"
+    git submodule add $url
+done
